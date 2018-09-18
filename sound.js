5db2511fbbf79b387698fc843462f3fc784fe526
madhur acharya <40020328+madhur-acharya@users.noreply.github.com>
2018-09-18 16:19:33 +0530

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,11 +1,14 @@
+var bgs_flag= false;
+
 var background_music= new Howl(
 {
     urls: ['resources/sound/background music.mp3'],
     loop: true,
     onload: function () 
     {
-    	background_music.volume(0.6);
-        //console.log("Loaded asset: background_music");
+        background_music.volume(0.6);
+        console.log("Loaded asset: background_music");
+        bg_flag= true;
     }
 });
 
@@ -14,7 +17,7 @@ var bullet_sound= new Howl(
     urls: ['resources/sound/bullets 3.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: bullet_sound");
+        console.log("Loaded asset: bullet_sound");
         bullet_sound.volume(0.1);
     }
 });
@@ -24,7 +27,7 @@ var asteroid_explosion_1= new Howl(
     urls: ['resources/sound/asteroid explosion 1.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: asteroid_explosion_1");
+        console.log("Loaded asset: asteroid_explosion_1");
         asteroid_explosion_1.volume(0.5);
     }
 });
@@ -34,7 +37,7 @@ var asteroid_explosion_2= new Howl(
     urls: ['resources/sound/asteroid explosion 2.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: asteroid_explosion_2");
+        console.log("Loaded asset: asteroid_explosion_2");
         asteroid_explosion_2.volume(0.5);
     }
 });
@@ -44,7 +47,7 @@ var enemy_explosion_1= new Howl(
     urls: ['resources/sound/enemy explosion 1.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: enemy_explosion_1");
+        console.log("Loaded asset: enemy_explosion_1");
         enemy_explosion_1.volume(1);
     }
 });
@@ -54,7 +57,7 @@ var powerup_1= new Howl(
     urls: ['resources/sound/powerup 1.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: powerup_1");
+        console.log("Loaded asset: powerup_1");
         powerup_1.volume(0.5);
     }
 });
@@ -64,7 +67,7 @@ var ship_movement_sound= new Howl(
     urls: ['resources/sound/ship movement sound.wav'],
     onload: function () 
     {
-       // console.log("Loaded asset: ship_movement_sound");
+        console.log("Loaded asset: ship_movement_sound");
         ship_movement_sound.volume(1);
     }
 });
@@ -74,7 +77,7 @@ var game_over_sound= new Howl(
     urls: ['resources/sound/game over.mp3'],
     onload: function () 
     {
-        //console.log("Loaded asset: game_over_sound");
+        console.log("Loaded asset: game_over_sound");
         game_over_sound.volume(1);
     }
 });
@@ -84,7 +87,7 @@ var bulletsplosion_sound= new Howl(
     urls: ['resources/sound/bulletsplosion.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: game_over_sound");
+        console.log("Loaded asset: game_over_sound");
         bulletsplosion_sound.volume(1);
     }
 });
@@ -94,7 +97,7 @@ var shield_collision= new Howl(
     urls: ['resources/sound/shield collision.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: shield_collision");
+        console.log("Loaded asset: shield_collision");
         shield_collision.volume(1);
     }
 });
@@ -104,7 +107,7 @@ var powerup_despawn_sound= new Howl(
     urls: ['resources/sound/powerup despawn.wav'],
     onload: function () 
     {
-        //console.log("Loaded asset: powerup_despawn_sound");
+        console.log("Loaded asset: powerup_despawn_sound");
         powerup_despawn_sound.volume(1);
     }
 });
